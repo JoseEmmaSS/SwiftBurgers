216dbddf0189ddb9060b6f1312f8216fafafbdbd
Antonio Medina Moreno <antoniomedinamoreno@micorreo.upp.edu.mx>
2023-07-17 00:53:58 -0600

    Mas mejoras de diseño

--- ClienteWeb/view/administracion/inventario/js/inventario.js
+++ ClienteWeb/view/administracion/inventario/js/inventario.js
@@ -12,15 +12,15 @@ const mostarData = (data) => {
 
   for (let i = 0; i < data.length; i++) {
     bodyInventario += `
-        <tr class="listaInventario border-b transition duration-300 ease-in-out hover:bg-neutral-100 dark:border-neutral-500 dark:hover:bg-neutral-600">
-          <td class="whitespace-nowrap dark:text-white px-6 py-4 font-medium">${data[i].idInventario}</td>
-          <td class="whitespace-nowrap dark:text-white px-6 py-4">${data[i].nombre}</td>
-          <td class="whitespace-nowrap dark:text-white px-6 py-4">${data[i].fechaCaducidad.substring(0, 10)}</td>
-          <td class="whitespace-nowrap dark:text-white px-6 py-4">${data[i].fechaIngreso.substring(0, 10)}</td>
-          <td class="whitespace-nowrap dark:text-white px-6 py-4">$ ${data[i].precio}</td>
-          <td class="whitespace-nowrap dark:text-white px-6 py-4">${data[i].cantidad}</td>
-          <td class="whitespace-nowrap dark:text-white px-6 py-4">${data[i].disponible ? 'SI' : 'NO'}</td>
-          <td class="whitespace-nowrap dark:text-white px-6 py-4">
+        <tr class="listaInventario border-b border-black duration-300 ease-in-out hover:bg-yellow-50 dark:border-neutral-500 dark:hover:bg-neutral-600">
+          <td class="whitespace-nowrap dark:text-white text-lg font-semibold px-6 py-4">${data[i].idInventario}</td>
+          <td class="whitespace-nowrap dark:text-white text-lg px-6 py-4">${data[i].nombre}</td>
+          <td class="whitespace-nowrap dark:text-white text-lg px-6 py-4">${data[i].fechaCaducidad.substring(0, 10)}</td>
+          <td class="whitespace-nowrap dark:text-white text-lg px-6 py-4">${data[i].fechaIngreso.substring(0, 10)}</td>
+          <td class="whitespace-nowrap dark:text-white text-lg px-6 py-4">$ ${data[i].precio}</td>
+          <td class="whitespace-nowrap dark:text-white text-lg px-6 py-4">${data[i].cantidad}</td>
+          <td class="whitespace-nowrap dark:text-white text-lg px-6 py-4">${data[i].disponible ? 'SI' : 'NO'}</td>
+          <td class="whitespace-nowrap dark:text-white text-lg px-6 py-4">
             <div class="flex space-x-4">
               <a class="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-2" id="editarInventario" onclick="editarInventario(${data[i].idInventario})">
                 <img class="h-4" src="../../../src/svg/editar.svg" alt="Editar">
