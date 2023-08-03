1b701da0abf92756f249b2b11992609a4075e520
Rafaelmag3 <rafaelmonrroy045@gmail.com>
2023-08-03 13:34:44 -0600

    Interfaces Carrito de compra

---
+++ Servidor/models/Venta.js
@@ -0,0 +1,33 @@
+const { DataTypes } = require('sequelize')
+const Platillo = require('./Platillo')
+const db = require('../config/db')
+
+const Venta = db.define('venta', {
+    idVenta: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    idPlatillo: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    cantidad: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    total: {
+        type: DataTypes.DECIMAL(10,2),
+        allowNull: false
+    }
+}, {
+    tableName: 'venta',
+    timestamps: false
+});
+
+Venta.belongsTo(Platillo, {
+    foreignKey: 'idPlatillo'
+});
+
+module.exports = Venta
